3293841e92a42eaa33f6ac34563c1ee3f1eec0fa
Alexandre Caramel <caramel.alexandre@gmail.com>
2020-09-25 10:36:07 +0200

    js/html/css

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const boutonREH = document.querySelector('.boutonREH');
+
+boutonREH.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    })
+
+
+}
